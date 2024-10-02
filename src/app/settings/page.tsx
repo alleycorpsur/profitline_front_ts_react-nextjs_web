6ba2e135b157efd17796e6e2055e28d38a3e8ece
mercuryeater <federicoc03@gmail.com>
2024-10-02 14:46:00 -0500

    refactor: :construction: addRoutesToMiddlewareSecurityAndAddSettingsRouting

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,9 @@
+import { ProjectsView } from "@/components/organisms/projects/ProjectsView/ProjectsView";
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Profitline"
+};
+
+export default function Home() {
+  return <ProjectsView />;
+}
