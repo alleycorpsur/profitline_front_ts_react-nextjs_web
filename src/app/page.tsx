97512a075d386e2ef2b7d7727ccdc010373524ec
Federico <federicoc03@gmail.com>
2024-10-03 00:11:53 -0500

    Remove unused page and layout components

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ProjectsView } from "@/components/organisms/projects/ProjectsView/ProjectsView";
-import { Metadata } from "next";
-export const metadata: Metadata = {
-  title: "Profitline"
-};
-
-export default function Home() {
-  return <ProjectsView />;
-}
