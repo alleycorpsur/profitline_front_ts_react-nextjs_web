d29baccec81ba56cbd58878a512041c920c615eb
Federico <federicoc03@gmail.com>
2024-12-03 18:13:18 -0500

    feat: enhance ModalCreateAdjustment with dynamic form fields and styling

--- src/modules/clients/containers/apply-tab/Modals/ModalCreateAdjustment/ModalCreateAdjustment.tsx
+++ src/modules/clients/containers/apply-tab/Modals/ModalCreateAdjustment/ModalCreateAdjustment.tsx
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
-import { Modal, Button, Form, Input, Spin } from "antd";
+import React, { useEffect } from "react";
+import { Modal } from "antd";
 import SecondaryButton from "@/components/atoms/buttons/secondaryButton/SecondaryButton";
 import PrincipalButton from "@/components/atoms/buttons/principalButton/PrincipalButton";
-import { CaretLeft } from "phosphor-react";
+import { CaretLeft, Plus } from "phosphor-react";
 import { Controller, useFieldArray, useForm } from "react-hook-form";
 import GeneralSelect from "@/components/ui/general-select";
 
 import "./modalCreateAdjustment.scss";
+import { InputFormMoney } from "@/components/atoms/inputs/InputFormMoney/InputFormMoney";
+import { InputForm } from "@/components/atoms/inputs/InputForm/InputForm";
 
 interface ISelect {
   value: string;
@@ -14,9 +16,9 @@ interface ISelect {
 }
 
 interface IAdjustment {
-  motive: ISelect;
-  detail: string;
-  amount: number;
+  motive?: ISelect;
+  detail?: string;
+  amount?: number;
 }
 interface ModalCreateAdjustmentProps {
   onCancel: () => void;
@@ -32,9 +34,6 @@ const ModalCreateAdjustment: React.FC<ModalCreateAdjustmentProps> = ({
   const {
     control,
     handleSubmit,
-    setValue,
-    watch,
-    trigger,
     reset,
     formState: { errors, isValid }
   } = useForm<{ adjustments: IAdjustment[] }>({
@@ -47,12 +46,22 @@ const ModalCreateAdjustment: React.FC<ModalCreateAdjustmentProps> = ({
     name: "adjustments"
   });
 
-  const isLoading = false;
+  useEffect(() => {
+    if (isOpen) {
+      reset(); // Clear the form when modal opens
+      append({ motive: undefined, detail: undefined, amount: undefined });
+    }
+  }, [isOpen, append, reset]);
+
+  const onSubmit = (data: { adjustments: IAdjustment[] }) => {
+    console.log("ajustes: ", data);
+  };
 
   return (
     <>
       <Modal
         className="modalCreateAdjustment"
+        width={"60%"}
         open={isOpen}
         onOk={onOk}
         closeIcon={false}
@@ -66,33 +75,64 @@ const ModalCreateAdjustment: React.FC<ModalCreateAdjustmentProps> = ({
 
         <div>
           {fields.map((field, index) => (
-            <Controller
-              key={field.id}
-              name={`adjustments.${index}.motive`}
-              control={control}
-              rules={{ required: "Cliente es obligatorio" }}
-              render={({ field }) => (
-                <GeneralSelect
-                  errors={errors?.adjustments?.[index]?.motive}
-                  field={field}
-                  title="Cliente"
-                  placeholder="Ingresar cliente"
-                  options={mockInputs}
-                  showSearch
-                  customStyleContainer={{ width: "100%" }}
-                  customStyleTitle={{ marginBottom: "4px" }}
-                />
-              )}
-            />
+            <div key={field.id} className="modalCreateAdjustment__adjustment">
+              <Controller
+                name={`adjustments.${index}.motive`}
+                control={control}
+                rules={{ required: "Cliente es obligatorio" }}
+                render={({ field }) => (
+                  <GeneralSelect
+                    errors={errors?.adjustments?.[index]?.motive}
+                    field={field}
+                    title="Cliente"
+                    placeholder="Ingresar cliente"
+                    options={mockInputs}
+                    showSearch
+                    customStyleContainer={{ width: "100%" }}
+                    customStyleTitle={{ marginBottom: "4px" }}
+                  />
+                )}
+              />
+
+              <InputForm
+                titleInput="Detalle"
+                control={control}
+                nameInput={`adjustments.${index}.detail`}
+                error={errors?.adjustments?.[index]?.detail}
+              />
+
+              <InputFormMoney
+                titleInput="Valor"
+                nameInput={`adjustments.${index}.amount`}
+                control={control}
+                error={errors?.adjustments?.[index]?.amount}
+                placeholder="Valor"
+                customStyle={{ width: "100%" }}
+                validationRules={{
+                  required: "Valor es obligatorio",
+                  min: { value: 1, message: "El valor debe ser mayor a 0" }
+                }}
+              />
+            </div>
           ))}
         </div>
+
+        <button
+          className="modalCreateAdjustment__addAdjustment"
+          onClick={() => append({ motive: undefined, detail: undefined, amount: undefined })}
+        >
+          <Plus />
+          Agregar pago
+        </button>
 
         <div className="modal-footer">
           <SecondaryButton fullWidth onClick={onCancel}>
             Cancelar
           </SecondaryButton>
 
-          <PrincipalButton fullWidth>Crear ajuste</PrincipalButton>
+          <PrincipalButton disabled={!isValid} onClick={handleSubmit(onSubmit)} fullWidth>
+            Crear ajuste
+          </PrincipalButton>
         </div>
       </Modal>
     </>
