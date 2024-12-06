6e4603a459f0efbd86bb3a5b1c295e9731d65df5
vickycabrera <vlcabrera92@hotmail.com>
2024-12-06 15:15:10 -0300

    add change status & delete discount packages - discounts

--- src/components/organisms/discounts/hooks/useDiscount.ts
+++ src/components/organisms/discounts/hooks/useDiscount.ts
@@ -3,7 +3,9 @@ import useSearch from "@/hooks/useSearch";
 import { useAppStore } from "@/lib/store/store";
 import {
   changeStatus,
+  changeStatusPackage,
   deleteDiscount,
+  deleteDiscountPackages,
   getAllDiscountPackages,
   getAllDiscounts
 } from "@/services/discount/discount.service";
@@ -19,7 +21,6 @@ type Props = {
   tabActive: string;
 };
 
-//type DiscountBasicsState = DiscountBasics & { checked: boolean };
 type DiscountBasicsState = DiscountBasics & { checked: boolean };
 type DiscountPackageState = DiscountPackage & { checked: boolean };
 
@@ -29,6 +30,7 @@ export default function useDiscount({ messageApi, tabActive }: Props) {
   const [isLoadingDelete, setIsLoadingDelete] = useState(false);
   const [active, setActive] = useState<number | undefined>(undefined);
   const [data, setData] = useState<any[]>([]);
+  console.log("data", data);
   const { searchQuery, handleChangeSearch } = useSearch();
   const { ID } = useAppStore((project) => project.selectedProject);
   const fetcher = tabActive === "1" ? getAllDiscountPackages : getAllDiscounts;
@@ -97,7 +99,15 @@ export default function useDiscount({ messageApi, tabActive }: Props) {
   const handleDeleteDiscount = async () => {
     try {
       setIsLoadingDelete(true);
-      await deleteDiscount(data.filter((item) => item.checked).map((item) => item.id));
+      // Filtrar los IDs de los elementos seleccionados
+      const selectedIds = data.filter((item) => item.checked).map((item) => item.id);
+
+      // Llamar a la función adecuada según `tabActive`
+      if (tabActive === "1") {
+        await deleteDiscountPackages(selectedIds);
+      } else {
+        await deleteDiscount(selectedIds);
+      }
     } catch (error) {
     } finally {
       handleClose();
@@ -116,6 +126,7 @@ export default function useDiscount({ messageApi, tabActive }: Props) {
   };
 
   const handleChangeStatus = async (id: number, newStatus: boolean) => {
+    let response;
     mutate(
       {
         ...res,
@@ -123,7 +134,7 @@ export default function useDiscount({ messageApi, tabActive }: Props) {
           if (item.id === id) {
             return {
               ...item,
-              status: newStatus
+              [tabActive === "1" ? "active" : "status"]: newStatus
             };
           }
           return item;
@@ -131,9 +142,19 @@ export default function useDiscount({ messageApi, tabActive }: Props) {
       } as GenericResponsePage<DiscountBasics[] | DiscountPackage[]>,
       { revalidate: false }
     );
-    const response = await changeStatus(id, newStatus);
+    if (tabActive === "1") {
+      response = await changeStatusPackage(id, newStatus);
+    } else {
+      response = await changeStatus(id, newStatus);
+    }
     if (response.success) {
-      messageApi.success(`Descuento ${newStatus ? "activado" : "desactivado"} con éxito`);
+      let message = "";
+      if (tabActive === "1") {
+        message = `Paquete de descuentos ${newStatus ? "activado" : "desactivado"} con éxito`;
+      } else {
+        message = `Descuento ${newStatus ? "activado" : "desactivado"} con éxito`;
+      }
+      messageApi.success(message);
     } else {
       mutate();
       messageApi.error(response.message);
