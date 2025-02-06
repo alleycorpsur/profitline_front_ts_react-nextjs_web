9a0f1718b73e44e819fc1b172a3d04a27ce3f869
Federico <federicoc03@gmail.com>
2025-02-05 22:19:17 -0500

    feat: :sparkles: add UsersProjectPage and remove DetailsProjectRouter for improved navigation

---
+++ src/app/proyectos/review/[id]/users/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+import UsersProjectView from "@/components/organisms/projects/UsersProjectVIew/UsersProjectView";
+
+const UsersProjectPage = () => {
+  return <UsersProjectView />;
+};
+
+export default UsersProjectPage;
