2f24791b755ef769c56a174cd75bafa301bb9e75
Marcos <84048657+Rodriguez-Marcos@users.noreply.github.com>
2025-02-21 14:59:16 -0300

    Revert "Update externalAuth.ts"
    This reverts commit 8579d99dc67828004cbc5ff82eb5edf3350af397.

--- src/services/externalAuth/externalAuth.ts
+++ src/services/externalAuth/externalAuth.ts
@@ -1,7 +1,6 @@
 import axios, { AxiosResponse } from "axios";
 import config from "@/config";
 import { ISendOtpResponse, IValidateOtpResponse } from "@/types/externalAuth/externalAuth";
-import { STORAGE_TOKEN } from "@/utils/constants/globalConstants";
 
 export const sendOtp = async (email: string, token: string): Promise<ISendOtpResponse> => {
   try {
@@ -24,13 +23,9 @@ export const sendOtp = async (email: string, token: string): Promise<ISendOtpRes
     console.warn("error sending otp: ", error);
     return error as any;
   }
-};
+}
 
-export const validateOtp = async (
-  email: string,
-  otp: string,
-  token: string
-): Promise<IValidateOtpResponse> => {
+export const validateOtp = async (email: string, otp: string, token: string): Promise<IValidateOtpResponse> => {
   try {
     const response: AxiosResponse<IValidateOtpResponse> = await axios.post(
       `${config.API_HOST}/email-otp/validate`,
@@ -46,21 +41,10 @@ export const validateOtp = async (
         }
       }
     );
-    await fetch("/api/auth", {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${response.data.data.auth}`
-      }
-    }).then(async (response) => {
-      const data = await response.json();
-      if (response.status === 200) {
-        localStorage.setItem(STORAGE_TOKEN, data.data.token);
-      }
-    });
 
     return response.data;
   } catch (error) {
     console.warn("error sending otp: ", error);
     return error as any;
   }
-};
+}
