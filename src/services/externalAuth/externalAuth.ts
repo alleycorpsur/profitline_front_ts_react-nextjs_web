96b0b8f975c8996b20fa3d7bf987e592844aa307
Federico <federicoc03@gmail.com>
2025-02-27 01:02:15 -0500

    refactor: remove token handling from OTP functions and use API instance for requests in LoginForm and externalAuth

--- src/services/externalAuth/externalAuth.ts
+++ src/services/externalAuth/externalAuth.ts
@@ -1,50 +1,30 @@
-import axios, { AxiosResponse } from "axios";
 import config from "@/config";
 import { ISendOtpResponse, IValidateOtpResponse } from "@/types/externalAuth/externalAuth";
+import { API } from "@/utils/api/api";
 
-export const sendOtp = async (email: string, token: string): Promise<ISendOtpResponse> => {
+export const sendOtp = async (email: string): Promise<ISendOtpResponse> => {
   try {
-    const response: AxiosResponse<ISendOtpResponse> = await axios.post(
-      `${config.API_HOST}/email-otp`,
-      {
-        email
-      },
-      {
-        headers: {
-          Accept: "application/json, text/plain, */*",
-          "Content-Type": "application/json; charset=utf-8",
-          Authorization: `${token}`
-        }
-      }
-    );
+    const response: ISendOtpResponse = await API.post(`${config.API_HOST}/email-otp`, {
+      email
+    });
 
-    return response.data;
+    return response;
   } catch (error) {
     console.warn("error sending otp: ", error);
     return error as any;
   }
-}
+};
 
-export const validateOtp = async (email: string, otp: string, token: string): Promise<IValidateOtpResponse> => {
+export const validateOtp = async (email: string, otp: string): Promise<IValidateOtpResponse> => {
   try {
-    const response: AxiosResponse<IValidateOtpResponse> = await axios.post(
-      `${config.API_HOST}/email-otp/validate`,
-      {
-        email,
-        otp
-      },
-      {
-        headers: {
-          Accept: "application/json, text/plain, */*",
-          "Content-Type": "application/json; charset=utf-8",
-          Authorization: `${token}`
-        }
-      }
-    );
+    const response: IValidateOtpResponse = await API.post(`${config.API_HOST}/email-otp/validate`, {
+      email,
+      otp
+    });
 
-    return response.data;
+    return response;
   } catch (error) {
     console.warn("error sending otp: ", error);
     return error as any;
   }
-}
+};
